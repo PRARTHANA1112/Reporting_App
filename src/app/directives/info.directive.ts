8fe079ee9dcc83cf8c31fd4fb07d11e00f5bad45
Prarthana <pj.prarthana@gmail.com>
2019-03-01 23:52:17 +0530

    Changes for directives

--- src/app/directives/info.directive.ts
+++ src/app/directives/info.directive.ts
@@ -10,8 +10,18 @@ export class InfoDirective {
   ngOnInit() { 
     //this.elementRef.nativeElement.innerHTML ='<h1>Hello World</h1>';
    }
+   
 
    @HostListener('mouseover') onMouseOver() { 
-    this.elementRef.nativeElement.innerHTML ='<h1>Hello World</h1>';
   }
+  @HostListener('mouseenter') onMouseenter(){
+    
+}
+@HostListener('mouseleave') onMouseLeave(){
+  this.elementRef.nativeElement.appendChild  ="<h1 style='background-color: #9e9e9e00;position: relative;font-size: 10px;text-align: center;float: left;left: 0px;border: 1px solid black;top: 50px;padding: 5px;'>"+
+  "Hello World</h1>";
+}
+
+  @HostListener('mouseout') onMouseOut() {
+    
 }
